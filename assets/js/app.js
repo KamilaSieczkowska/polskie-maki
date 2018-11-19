873c03605bcc833eb8180ed74eafcc18c99f1238
Kamila <kamila.sieczkowska@gmail.com>
2018-11-19 23:55:25 +0100

    still working on mockup version

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,16 @@ $( document ).ready(function() {
         autoplaySpeed: 5000
     });
 
-
+    $('#load-project-info').on('click', function(){
+        var projectDiv = $('#project-info');
+        
+        if (projectDiv.hasClass('opened')){
+            projectDiv.removeClass('opened');
+            $(this).find('a').text('Dowiedz się więcej');
+        } else {
+            projectDiv.addClass('opened');
+            $(this).find('a').text('Zwiń');
+        }
+    })
 
 });
