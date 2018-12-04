424d63307b1f73ee1c9a254b2f19d58dac23c945
Kamila Sieczkowska <kamila.sieczkowska@closbrothers.pl>
2018-12-04 17:23:42 +0100

    working on mobile version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,47 @@
 $( document ).ready(function() {
 
+
+    // TOGGLE MOBILE MENU
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
+        $('#mobile__menu').toggleClass('open');
+    });
+
+    //HIDE MOBILE MENU ON ITEM CLICK
+    var mobileMenuLinks = $('#mobile__menu').find('.menu__item');
+    mobileMenuLinks.each(function(){
+        $(this).click(function(){
+            if($('#toggle').hasClass('active')) {
+                setTimeout(function(){
+                    $('#toggle').removeClass('active');
+                    $('#mobile__menu').removeClass('open');
+                },300)
+            }
+        })
+    })
+
+    // SCROLL NAVI EFFECT
+    $(document).scroll(function(){
+        var $nav = $(".navbar");
+        $nav.toggleClass('after-scroll', $(this).scrollTop() > $nav.height());
+    });
+
+    // SLICK SLIDER INIT
     $('#header-slider').slick({
         autoplay: true,
         autoplaySpeed: 5000
     });
 
-    $(document).scroll(function(){
-        var $nav = $(".navbar");
-        $nav.toggleClass('after-scroll', $(this).scrollTop() > $nav.height());
-    })
-
+    // LOAD PROJECT INFO
     $('#load-project-info').on('click', function(){
         var projectDiv = $('#project-info');
         
         if (projectDiv.hasClass('opened')){
             projectDiv.removeClass('opened');
-            $(this).find('a').text('Dowiedz się więcej');
+            $(this).find('span').text('Dowiedz się więcej');
         } else {
             projectDiv.addClass('opened');
-            $(this).find('a').text('Zwiń');
+            $(this).find('span').text('Zwiń');
         }
     })
 
